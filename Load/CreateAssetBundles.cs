8dd00a23a065899517709c302cceaeaf389b6f64
czlilove <czlelove@hotmail.com>
2018-01-27 12:53:51 +0800

    download

---
+++ Load/CreateAssetBundles.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using System.IO;
+using UnityEngine;
+
+public class CreateAssetBundles
+{
+	[MenuItem("Assets/Build AssetBundles")]
+	static void BuildAllAssetBundles()
+	{
+		string assetBundleDirectory = "Assets/TestAB";
+		if(!Directory.Exists(assetBundleDirectory))
+		{
+			Directory.CreateDirectory(assetBundleDirectory);
+		}
+		BuildTarget currentTarget = EditorUserBuildSettings.activeBuildTarget;
+		Debug.LogError ("currentTarget is:"+currentTarget);
+		AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, currentTarget);
+		string[] all = manifest.GetAllAssetBundles ();
+		Debug.LogError (all.Length);
+		Debug.LogError (all[0]);
+		Debug.LogError (all[1]);
+
+		string[] all01 = manifest.GetAllDependencies (all[0]);
+		string[] all02 = manifest.GetDirectDependencies (all[0]);
+		string[] all11 = manifest.GetAllDependencies (all[1]);
+	}
+}
